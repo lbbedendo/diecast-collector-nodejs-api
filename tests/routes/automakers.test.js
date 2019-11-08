bad4c87c6a5f61c5c9c2e2fabb93de41f198e20b
Leonardo Barbieri <leonardobdn@gmail.com>
2019-11-08 17:23:54 -0300

    Add tests for automakers controller

--- tests/routes/automakers.test.js
+++ tests/routes/automakers.test.js
@@ -1,1 +1,26 @@
 const request = require("supertest");
+const app = require("../../src/server");
+
+describe("Automaker endpoints", () => {
+  it("should create a new automaker", async () => {
+    const res = await request(app)
+      .post("/automakers")
+      .send({
+        name: "Honda",
+        country: "Japan"
+      });
+    expect(res.statusCode).toEqual(201);
+  });
+
+  it("should return all automakers", async () => {
+    const res = await request(app).get("/automakers");
+    expect(res.statusCode).toEqual(200);
+  });
+
+  it("should return one automaker", async () => {
+    const res = await request(app).get("/automakers/1");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty("name");
+    expect(res.body).toHaveProperty("country");
+  });
+});
